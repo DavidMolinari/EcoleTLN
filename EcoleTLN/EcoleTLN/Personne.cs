37f257c5d6102421a1313ae4ca71fd66da9f1d37
Sevenn <thomas.s833@gmail.com>
2016-03-16 09:15:31 +0100

    Using inutile

--- EcoleTLN/EcoleTLN/Personne.cs
+++ EcoleTLN/EcoleTLN/Personne.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EcoleTLN
+﻿namespace EcoleTLN
 {
     public abstract class Personne
     {
