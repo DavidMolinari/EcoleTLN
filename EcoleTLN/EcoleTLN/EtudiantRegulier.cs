0e1eb19f7a159404daed8acc80d82afe76e01b39
Sevenn <thomas.s833@gmail.com>
2016-03-16 09:14:46 +0100

    class etudiantRegulier terminé

--- EcoleTLN/EcoleTLN/EtudiantRegulier.cs
+++ EcoleTLN/EcoleTLN/EtudiantRegulier.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace EcoleTLN
 {
+    class EtudiantReguliuer : Etudiant
+    {
+        public EtudiantReguliuer(string section, double moyenne, string nom, int annee):base(moyenne, section, nom, annee)
+        {
+        }
+    }
  
 }
